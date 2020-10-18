8f9d586108af22ed6f6d5418de933a0b99c3d215
J-Waz <jgw.jakegeorge@gmail.com>
2020-10-18 15:32:05 -0500

    color changer and square adjuster done

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Text, View, StyleSheet, Button } from 'react-native'
 
 const CounterScreen = () => {
+    const [counter, setCounter] = useState(0)
+
+
         return (
             <View>
-                <Text> textInComponent </Text>
+            <Button title="increase" onPress={() => {setCounter(counter + 1)}} />
+            <Button title="decrease" onPress={() => {setCounter(counter - 1)}}/>
+                <Text> Current Count:  {counter}</Text>
             </View>
         )
     }
